f38a6db457f35cca723df5e7c8e11e965c08c411
Vinicius <viniaoliveira22@gmail.com>
2024-05-24 21:00:05 -0300

    first commit project initialize

---
+++ src/main/java/br/com/engvini/bazar_virtual/BazarVirtualApplication.java
@@ -0,0 +1,13 @@
+package br.com.engvini.bazar_virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/engvini/bazar_virtual/BazarVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.engvini.bazar_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
